0e6c40d6c9600c6d06e53ca82e099ce91760c34e
GUODnuli <624115642@qq.com>
2024-10-10 00:41:51 +0800

    Update.

--- Src/Client/Assets/Scripts/Managers/GameObjectManager.cs
+++ Src/Client/Assets/Scripts/Managers/GameObjectManager.cs
@@ -35,6 +35,38 @@ public class GameObjectManager : MonoBehaviour
 
     IEnumerator InitGameObjects()
     {
-        if ()
+        foreach (var cha in CharacterManager.Instance.Characters.Values)
+        {
+            CreateCharacterObject(cha);
+            yield return null;
+        }
+    }
+
+    private void CreateCharacterObject(Character character)
+    {
+        if (!Characters.ContainsKey(character.Info.Id) || Characters[character.Info.Id] == null)
+        {
+            Object obj = Resloader.Load<Object>(character.Define.Resource);
+            if(obj == null)
+            {
+                Debug.LogErrorFormat("Character: [{0}], Resource: [{1}] not existed.", character.Define.TID, character.Define.Resource);
+                return;
+            }
+            GameObject go = (GameObject)Instantiate(obj);
+            go.name = "Character_" + character.Info.Id + "_" + character.Info.Name;
+
+            go.transform.position = GameObjectTool.LogicToWorld(character.position);
+            go.transform.forward = GameObjectTool.LogicToWorld(character.direction);
+            Characters[character.Info.Id] = go;
+
+            EntityController ec = go.GetComponent<EntityController>();
+            if (ec != null)
+            {
+                ec.entity = character;
+                ec.isPlayer = character.IsPlayer;
+            }
+            
+            PlayerInputController pc = go.Get
+        }
     }
 }
