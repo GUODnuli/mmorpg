6c1ec7302af1c6c470866c3d8d2b74fd50147261
zhuangxiaosen <xz.ganbaruibro@gmail.com>
2024-11-01 12:09:01 +0800

    Add Iterms DataStruct.

--- Src/Data/Excel2Json.cmd
+++ Src/Data/Excel2Json.cmd
@@ -1,3 +1,4 @@
+chcp 65001 > nul
 json-excel\json-excel json Tables\ Data\
 
 REM 检查并创建 Client\Data 目录
@@ -17,11 +18,13 @@ if not exist "..\Server\GameServer\GameServer\bin\Debug\Data\" (
 @copy Data\LevelUpDefine.txt ..\Client\Data\
 @copy Data\SpawnRuleDefine.txt ..\Client\Data\
 @copy Data\NPCDefine.txt ..\Client\Data\
+@copy Data\ItemDefine.txt ..\Client\Data\
 
 @copy Data\CharacterDefine.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
 @copy Data\MapDefine.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
 @copy Data\LevelUpDefine.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
 @copy Data\SpawnRuleDefine.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
 @copy Data\NPCDefine.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
+@copy Data\ItemDefine.txt ..\Server\GameServer\GameServer\bin\Debug\Data\
 
 pause
