b157c755f36884af86fd76e9dcd0b25b32d64121
GUODnuli <624115642@qq.com>
2025-05-31 23:21:06 +0800

    GuildSystem coding.

---
+++ Src/Client/Assets/Scripts/UI/Guild/UIGuild.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Services;
+using Managers;
+
+public class UIGuild : UIWindow
+{
+    public GameObject itemPrefab;
+    public ListView listMain;
+    public Transform itemRoot;
+    public UIGuildInfo uiInfo;
+    public UIGuildItem
+}
